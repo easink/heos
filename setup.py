06d7b659753b558860024e27fa8ba2a6419435d0
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2016-08-14 21:59:31 +0200

    try to make package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='heos',
+      version=open('heos/version.py').read()
+      description='Denon Heos',
+      url='http://github.com/andryd/heos',
+      author='Andreas Rydbrink',
+      author_email='andreas.rydbrink@gmail.com',
+      license='MIT',
+      packages=['heos'],
+      long_description=open('README.md').read(),
+      install_requires=[],
+      zip_safe=False)
